b893241797730897431f33a7bfa9bbc11c23d917
FillMember <fillmember@gmail.com>
2016-08-05 11:07:00 +0200

    add preset image

--- src/js/SimulationPresets.js
+++ src/js/SimulationPresets.js
@@ -2,61 +2,73 @@ module.exports = [
     { 
         name: "Default",
         feed: 0.37,
-        kill: 0.6
+        kill: 0.6,
+        image : 'assets/preset-image/default.png'
     },
     { 
         name: "Solitons",
         feed: 0.3,
-        kill: 0.62
+        kill: 0.62,
+        image : 'assets/preset-image/solitons.png'
     },
     { 
         name: "Pulsating Solitons",
         feed: 0.25,
-        kill: 0.6
+        kill: 0.6,
+        image : 'assets/preset-image/pulsing-solitons.png'
     },
     { 
         name: "Worms",
         feed: 0.78,
-        kill: 0.61
+        kill: 0.61,
+        image : 'assets/preset-image/worms.png'
     },
     { 
         name: "Mazes",
         feed: 0.29,
-        kill: 0.57
+        kill: 0.57,
+        image : 'assets/preset-image/maze.png'
     },
     { 
         name: "Holes",
         feed: 0.39,
-        kill: 0.58
+        kill: 0.58,
+        image : 'assets/preset-image/holes.png'
     },
     { 
         name: "Chaos",
         feed: 0.26,
-        kill: 0.51
+        kill: 0.51,
+        image: 'assets/preset-image/chaos.png'
     },
     { 
         name: "Chaos & Holes",
         feed: 0.34,
-        kill: 0.56
+        kill: 0.56,
+        image: 'assets/preset-image/chaos-holes.png'
     },
     { 
         name: "Moving Spots",
         feed: 0.14,
-        kill: 0.54
+        kill: 0.54,
+        image: 'assets/preset-image/spots.png'
     },
     { 
         name: "Spots & Loops",
         feed: 0.18,
-        kill: 0.51
+        kill: 0.51,
+        image: 'assets/preset-image/spots-loops.png'
     },
     { 
         name: "Waves",
         feed: 0.14,
-        kill: 0.45
+        kill: 0.45,
+        image: 'assets/preset-image/waves.png'
     },
     { 
         name: "U-Skate",
         feed: 0.62,
-        kill: 0.6093
+        kill: 0.6093,
+        image: 'assets/preset-image/u-skate.png'
     }
 ]
